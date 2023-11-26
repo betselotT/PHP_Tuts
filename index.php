824383e7acd9bffa9354b63574aaa582b9448399
Betselot Tesfa <betselottesfa9@gmail.com>
2023-11-26 08:58:23 -0900

    examples

--- index.php
+++ index.php
@@ -1,5 +1,32 @@
 <?php
     echo 'hello world';
+    $name = 'Aman';
+    echo "<br/>";
+    echo $name;
+    $age = 20;
+
+    define('NAME', 'Aman');
+
+    $stringOne = 'My email is ';
+    $stringTwo = 'betselot@gmail.com';
+    echo "<br/>";
+    echo $stringOne . $stringTwo;
+    echo "<br/>";
+
+    $name = 'Betsi';
+    echo 'Hey, my name is ' . $name;
+    echo "<br/>";
+    echo "Hey, my name is $name";
+    echo "<br/>";
+    echo "We are in \"AASTU\"";
+    echo "<br/>";
+    echo 'We are in "AASTU"';
+    echo "<br/>";
+    echo $name[2];
+
+    echo strlen($name);
+    echo strtoupper($name);
+    echo str_replace('i', 'u', $name);
 ?>
 
 <!DOCTYPE html>
@@ -11,5 +38,8 @@
 </head>
 <body>
     <h1><?php echo 'hello world' ?></h1>
+    <h1><?php echo $name ?></h1>
+    <h1><?php echo $age ?></h1>
+    <h1><?php echo NAME ?></h1>
 </body>
 </html>
